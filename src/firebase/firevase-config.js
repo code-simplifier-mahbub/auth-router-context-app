445c691cfa51386fcccb9e57dd5efca9b6f6dbb3
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-11-07 12:50:16 +0600

    create context for share value in other component

---
+++ src/firebase/firevase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXoBUZ50O1XTgb2gvOR-r0XzANdSrlRvU",
+  authDomain: "auth-router-context-57ba2.firebaseapp.com",
+  projectId: "auth-router-context-57ba2",
+  storageBucket: "auth-router-context-57ba2.appspot.com",
+  messagingSenderId: "159550781180",
+  appId: "1:159550781180:web:4c8f076f55ae0ea4320979"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
